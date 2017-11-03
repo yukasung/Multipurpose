2ae5011e7e469572bef70212ca8374a5d281e745
Chainarong Jaykeaw <chainarong.j@Chainarongs-MacBook-Pro.local>
2017-11-03 19:10:29 +0700

    no message

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -7,7 +7,7 @@ var browserSync = require('browser-sync');
 var plumber = require('gulp-plumber');
 var path = require('path');
 var notify = require('gulp-notify');
-var headerfooter = require('gulp-headerfooter');
+var nunjucksRender = require('gulp-nunjucks-render');
 
 // var cssbeautify = require('gulp-cssbeautify');
 // var bulkSass = require('gulp-sass-bulk-import');
@@ -17,7 +17,7 @@ path = require('path');
 
 //Autoprefixer
 var browser_support = [
-    'ie >= 10',    
+    'ie >= 10',
     'ff >= 31',
     'chrome >= 36',
     'safari >= 6',
@@ -32,15 +32,15 @@ var notifyInfo = {
 };
 
 //error notification settings for plumber
-var plumberErrorHandler = { errorHandler: notify.onError({
+var plumberErrorHandler = {
+    errorHandler: notify.onError({
         title: notifyInfo.title,
         icon: notifyInfo.icon,
         message: "Error: <%= error.message %>"
     })
 };
 
-
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync({
         server: {
             baseDir: "./"
@@ -49,7 +49,7 @@ gulp.task('browser-sync', function() {
 });
 
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     gulp.src('scss/**/*.scss')
         .pipe(plumber(plumberErrorHandler))
         // .pipe(sourcemaps.init())
@@ -58,27 +58,25 @@ gulp.task('sass', function() {
         .pipe(autoprefixer(browser_support))
         // .pipe(cssbeautify())
         .pipe(gulp.dest('assets/css'))
-        // .pipe(livereload());
-});    
+    // .pipe(livereload());
+});
 
-gulp.task('master', function () {
-    gulp.src('content/*.html')
-        .pipe(headerfooter.header('master/header.html'))
-        .pipe(headerfooter.footer('master/footer.html'))
+gulp.task('nunjucks', function () {
+    return gulp.src(['pages/**/*.html', '!pages/layout.html'])
+        .pipe(nunjucksRender({
+            path: ['templates']
+        }))
         .pipe(gulp.dest('./'))
 });
 
-
 // Default
-gulp.task('default', ['master', 'sass', 'browser-sync'], function() {  
-      
-    
+gulp.task('default', ['nunjucks', 'sass', 'browser-sync'], function () {
+
     gulp.watch('scss/**/*.scss', ['sass']);
-    gulp.watch('master/**/*.html',['master']);
-    gulp.watch('content/**/*.html',['master']);
+    gulp.watch(['pages/**/*.html', 'templates/**/**.html'], ['nunjucks']);
 
     gulp.watch(['./*.html'], browserSync.reload);
     gulp.watch(['assets/js/**/*.js'], browserSync.reload);
-    gulp.watch(['assets/css/**/*.css'], browserSync.reload); 
-    
+    gulp.watch(['assets/css/**/*.css'], browserSync.reload);
+
 });
