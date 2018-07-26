fbaebbdd0b2fc7a1ab48ddaa9e0dab1ff02d3167
Chainarong Jaykeaw <>
2018-07-26 16:32:49 +0700

    no message

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -244,134 +244,21 @@ var theme = {
     },
     initGoogleMap: function () {
 
-        (function ($, window, document, undefined) {
-            var $window = $(window),
-                mapInstances = [],
-                $pluginInstance = $('.google-map').lazyLoadGoogleMaps({
-                    callback: function (container, map) {
-                        var $container = $(container),
-                            center = new google.maps.LatLng($container.attr('data-lat'), $container.attr('data-lng'));
+        $('.google-map').lazyLoadGoogleMaps({
+            callback: function (container, map) {
+                var $container = $(container),
+                    center = new google.maps.LatLng($container.attr('data-lat'), $container.attr('data-lng'));
 
-                        map.setOptions({
-                            center: center,
-                            zoom: 15,
-                            zoomControl: true,
-                            zoomControlOptions: {
-                                style: google.maps.ZoomControlStyle.DEFAULT,
-                            },
-                            disableDoubleClickZoom: false,
-                            mapTypeControl: true,
-                            mapTypeControlOptions: {
-                                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-                            },
-                            scaleControl: true,
-                            scrollwheel: false,
-                            streetViewControl: true,
-                            draggable: true,
-                            overviewMapControl: false,
-                            mapTypeId: google.maps.MapTypeId.ROADMAP,
-                            styles: [{
-                                stylers: [{
-                                    saturation: -100
-                                }, {
-                                    gamma: 1
-                                }]
-                            }, {
-                                elementType: "labels.text.stroke",
-                                stylers: [{
-                                    visibility: "off"
-                                }]
-                            }, {
-                                featureType: "poi.business",
-                                elementType: "labels.text",
-                                stylers: [{
-                                    visibility: "off"
-                                }]
-                            }, {
-                                featureType: "poi.business",
-                                elementType: "labels.icon",
-                                stylers: [{
-                                    visibility: "off"
-                                }]
-                            }, {
-                                featureType: "poi.place_of_worship",
-                                elementType: "labels.text",
-                                stylers: [{
-                                    visibility: "off"
-                                }]
-                            }, {
-                                featureType: "poi.place_of_worship",
-                                elementType: "labels.icon",
-                                stylers: [{
-                                    visibility: "off"
-                                }]
-                            }, {
-                                featureType: "road",
-                                elementType: "geometry",
-                                stylers: [{
-                                    visibility: "simplified"
-                                }]
-                            }, {
-                                featureType: "water",
-                                stylers: [{
-                                    visibility: "on"
-                                }, {
-                                    saturation: 50
-                                }, {
-                                    gamma: 0
-                                }, {
-                                    hue: "#50a5d1"
-                                }]
-                            }, {
-                                featureType: "administrative.neighborhood",
-                                elementType: "labels.text.fill",
-                                stylers: [{
-                                    color: "#333333"
-                                }]
-                            }, {
-                                featureType: "road.local",
-                                elementType: "labels.text",
-                                stylers: [{
-                                    weight: 0.5
-                                }, {
-                                    color: "#333333"
-                                }]
-                            }, {
-                                featureType: "transit.station",
-                                elementType: "labels.icon",
-                                stylers: [{
-                                    gamma: 1
-                                }, {
-                                    saturation: 50
-                                }]
-                            }]
-                        });
-                        new google.maps.Marker({
-                            position: center,
-                            map: map
-                        });
-
-                        $.data(map, 'center', center);
-                        mapInstances.push(map);
-
-                        var updateCenter = function () {
-                            $.data(map, 'center', map.getCenter());
-                        };
-                        google.maps.event.addListener(map, 'dragend', updateCenter);
-                        google.maps.event.addListener(map, 'zoom_changed', updateCenter);
-                        google.maps.event.addListenerOnce(map, 'idle', function () {
-                            $container.addClass('is-loaded');
-                        });
-                    }
+                map.setOptions({
+                    zoom: 15,
+                    center: center
                 });
-
-            $window.on('resize', $pluginInstance.debounce(1000, function () {
-                $.each(mapInstances, function () {
-                    this.setCenter($.data(this, 'center'));
+                new google.maps.Marker({
+                    position: center,
+                    map: map
                 });
-            }));
-
-        })(jQuery, window, document);
+            }
+        });
 
     },
     initProgressBar: function () {
