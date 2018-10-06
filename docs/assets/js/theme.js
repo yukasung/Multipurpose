7c4142aba6e3ba361b3f66522d9826139b99bdbf
Chainarong Jaykeaw <>
2018-10-06 18:52:39 +0700

    no message

---
+++ docs/assets/js/theme.js
@@ -0,0 +1,254 @@
+"use strict";
+
+var theme = {
+    ButtonGoTopOffset: 100,
+    ButtonGoTopDuration: 500,
+    init: function () {
+
+        theme.initGoogleMap();
+        theme.initProgressBar();
+        theme.initCheckboxCollapse();
+        theme.initOwlCarousel();
+        theme.initParallax();
+        theme.initVideoCover();
+        theme.initChart();
+        theme.initCounter();
+        theme.initGoToTopBotton();
+
+    },
+    loadEvent: function () {
+
+        $('#preloader .loading').fadeOut();
+        $("#preloader").fadeOut(function () {
+            $(this).remove();
+        });
+        $('body').css({
+            'overflow': 'visible'
+        });
+
+    },
+    resizeEvent: function () {
+        this.initVideoCover();
+    },
+    scrollEvent: function (obj) {
+
+        if ($(obj).scrollTop() > this.ButtonGoTopOffset) {
+            $('#btnGoTop').fadeIn(this.ButtonGoTopDuration);
+        } else {
+            $('#btnGoTop').fadeOut(this.ButtonGoTopDuration);
+        }
+
+    },
+    initParallax: function () {
+
+        if ($('.parallax').length > 0) {
+            new Rellax('.parallax', {
+                center: true
+            });
+        }
+
+    },
+    initChart: function () {
+
+        $('#chartBar').each(function (i) {
+
+            $(this).appear(function () {
+                
+                Chart.defaults.global.defaultFontColor = '#333333';
+                var chartBar = $("#chartBar")[0].getContext('2d');
+
+                new Chart(chartBar, {
+                    type: 'bar',
+                    data: {
+                        labels: ["2012", "2013", "2014", "2015", "2016", "2017", "2018"],
+                        datasets: [{
+                            label: 'Revenue by Year',
+                            data: [22, 24, 25, 26, 29, 31, 34],
+                            backgroundColor: 'rgba(52, 152, 219, 1)',
+                            borderWidth: 0
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        title: {
+                            display: true,
+                            text: 'Revenue by Year'
+                        },
+                        legend: {
+                            display: false,
+                        },
+                        scales: {
+                            yAxes: [{
+                                display: true,
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: 'Revenue in billion U.S. dollars'
+                                },
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        },
+                        animation: {
+                            duration: 3000
+                        }
+                    }
+                });
+                
+            });
+
+        });
+
+    },
+    initOwlCarousel: function () {
+
+        var owl = $('.owl-carousel');
+        owl.imagesLoaded(function () {
+            owl.each(function () {
+
+                var options = $(this).data("owl-carousel-options");
+                var defaults = {
+                    navText: ["<img src='../../assets/images/other/nav_prev.png'>", "<img src='../../assets/images/other/nav_next.png'>"]
+                }
+
+                $(this).owlCarousel($.extend(defaults, options));
+
+            });
+        });
+    },
+    initCounter: function () {
+
+        $(".counter").appear(function () {
+            $(this).children('.text-counter').countTo();
+        });
+
+    },
+    initGoogleMap: function () {
+
+        $('.google-map').lazyLoadGoogleMaps({
+
+            callback: function (container, map) {
+
+                var $container = $(container);
+                var $center = new google.maps.LatLng($container.attr('data-lat'), $container.attr('data-lng'));
+                var $zoom = 15;
+
+                if ($container.attr('data-zoom')) {
+                    $zoom = parseInt($container.attr('data-zoom'));
+                }
+                map.setOptions({
+                    zoom: $zoom,
+                    center: $center
+                });
+                new google.maps.Marker({
+                    position: $center,
+                    map: map
+                });
+
+            }
+
+        });
+
+    },
+    initProgressBar: function () {
+
+        $('.skillbar').each(function (i) {
+            $(this).appear(function () {
+                $(this).skillBars({
+                    from: 0,
+                    speed: 2000,
+                    interval: 100,
+                });
+            });
+        });
+
+    },
+    initCheckboxCollapse: function () {
+
+        $('.checkbox-collapse .custom-checkbox').on('click', function (e) {
+
+            if ($(this).find('input[type=checkbox]').prop('checked')) {
+                $(this).parent().find('.collapse').collapse('show');
+            } else {
+                $(this).parent().find('.collapse').collapse('hide');
+            }
+
+            e.stopPropagation();
+
+        })
+
+    },
+    initVideoCover: function () {
+
+        var videoCover = $('.video-cover');
+        var image = videoCover.find('img');
+
+        videoCover.removeAttr('style');
+
+        videoCover.imagesLoaded(function () {
+            image.each(function () {
+
+                if (videoCover.hasClass('video-reveal')) {
+                    image.removeClass('d-none');
+                    videoCover.css({
+                        'width': $(this).width(),
+                        'height': $(this).height(),
+                    });
+                    image.addClass('d-none');
+                } else {
+                    videoCover.css({
+                        'width': $(this).width(),
+                        'height': $(this).height(),
+                    });
+                }
+
+            });
+        });
+
+        $('.video-cover .video-icon').click(function (e) {
+
+            e.preventDefault();
+
+            var video = $(this).closest('.video-cover');
+            var image = video.find('img');
+            var iframe = video.find('iframe');
+            var embed = video.find('.embed-responsive');
+            var src = iframe.data('src');
+
+            image.addClass('d-none');
+            embed.removeClass("d-none");
+            video.addClass('video-reveal');
+            iframe.attr('src', src);
+
+        });
+
+    },
+    initGoToTopBotton() {
+
+        $('#btnGoTop').click(function (event) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, this.ButtonGoTopDuration);
+            return false;
+        });
+
+    }
+};
+
+
+$(document).ready(function () {
+    theme.init();
+});
+
+$(window).resize(function () {
+    theme.resizeEvent();
+});
+
+$(window).scroll(function () {
+    theme.scrollEvent(this);
+});
+
+$(window).load(function () {
+    theme.loadEvent();
+});
---
+++ docs/assets/vendor/bootstrap-navbar.js
@@ -0,0 +1,162 @@
+"use strict";
+
+var navbar = {
+    LastScroll: 0,
+    init: function () {
+
+        var slideDownDuration = 100;
+        var slideUpDuration = 50;
+
+        $('.navbar-dropdown-hover .dropdown-menu').on('click', function (e) {
+            e.stopPropagation();
+        });
+
+        $('#navbarCollapse').on('show.bs.collapse', function () {
+            $('header').addClass('navbar-collapse-show');
+            $('.navbar-action').css('transition', '');
+        });
+
+        $('#navbarCollapse').on('hide.bs.collapse', function () {
+            $('header').removeClass('navbar-collapse-show');
+        });
+
+        if (!/Mobi|Android/i.test(navigator.userAgent)) {
+            $('.navbar-dropdown-hover .dropdown').mouseenter(this.dropdownHandlerIn).mouseleave(this.dropdownHandlerOut);
+        }
+
+        $('.navbar .dropdown-menu .dropdown-toggle').on('click', function (e) {
+
+            var $el = $(this);
+            var $parent = $(this).offsetParent(".dropdown-menu");
+            var $subMenu = $(this).next(".dropdown-menu");
+
+            if (!$(this).next().hasClass('show')) {
+                $(this).parents('.dropdown-menu').first().find('.show').removeClass("show").css({
+                    'display': ''
+                });
+            }
+
+            if ($subMenu.hasClass('show')) {
+                $subMenu.stop(true, true).slideUp(slideUpDuration);
+            } else {
+                $subMenu.stop(true, true).slideDown(slideDownDuration);
+            }
+
+            $subMenu.toggleClass('show');
+
+            $(this).parent("li").toggleClass('show');
+            $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+                $('.dropdown-menu .show').removeClass("show").css({
+                    'display': ''
+                });
+            });
+
+            if (!$parent.parent().hasClass('navbar-nav') && !$('.navbar-toggler').is(':visible')) {
+
+                $el.next().css({
+                    "top": $el[0].offsetTop
+                });
+
+                if ($parent.hasClass('dropdown-left')) {
+                    $el.next().css({
+                        "left": $parent.outerWidth() * -1
+                    });
+                } else {
+                    $el.next().css({
+                        "left": $parent.outerWidth()
+                    });
+                }
+
+            }
+
+            return false;
+
+        });
+
+        $('.dropdown').on('show.bs.dropdown', function (e) {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideDown(slideDownDuration);
+        });
+
+        $('.dropdown').on('hide.bs.dropdown', function (e) {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideUp(slideUpDuration);
+        });
+
+    },
+    dropdownHandlerIn: function () {
+
+        if (!$('.navbar-toggler').is(':visible')) {
+            $(this).find('.dropdown-toggle').first().trigger('click');
+        }
+
+    },
+    dropdownHandlerOut: function () {
+
+        if (!$('.navbar-toggler').is(':visible')) {
+            if ($(this).hasClass('show')) {
+                $(this).find('.dropdown-toggle').first().trigger('click');
+            }
+        }
+
+    },
+    eventResize: function () {
+
+        if ($('.navbar-toggler').is(":visible")) {
+            $('.navbar-action').css('transition', 'none');
+        } else {
+            $('.navbar-action').css('transition', '');
+            $('header').removeClass('navbar-collapse-show');
+        }
+
+    },
+    eventScroll: function (element) {
+
+        var navbar = $('.navbar');
+
+        if (navbar.hasClass('fixed-hide')) {
+            // sticky nav start
+            var headerHeight = $('nav').outerHeight();
+
+            if ($(document).scrollTop() >= headerHeight) {
+                if (!navbar.hasClass('sticky')) {
+                    navbar.addClass('sticky');
+                }
+            } else if ($(document).scrollTop() <= headerHeight) {
+                if (navbar.hasClass('sticky')) {
+                    navbar.removeClass('sticky');
+                }
+            }
+
+            // header appear on scroll up
+            var st = element.scrollTop();
+            if (st > this.LastScroll) {
+                if ($('.sticky').hasClass('show')) {
+                    $('.sticky').removeClass('show');
+                }
+            } else {
+                if (!$('.sticky').hasClass('show')) {
+                    $('.sticky').addClass('show');
+                }
+            }
+
+            this.LastScroll = st;
+            if (this.LastScroll <= headerHeight) {
+                if (navbar.hasClass('show')) {
+                    navbar.removeClass('show');
+                }
+            }
+        }
+
+    }
+};
+
+$(document).ready(function () {
+    navbar.init();
+});
+
+$(window).resize(function () {
+    navbar.eventResize();
+});
+
+$(window).scroll(function () {
+    navbar.eventScroll($(this));
+});
